9e630bff29e6195040ab0a61ea00d1dad9edf9a4
Patric <patric.lessmann@gmail.com>
2013-12-06 11:39:45 +0100

    error corrections concerning postsharp

--- CarSharing/Controllers/AdminController.cs
+++ CarSharing/Controllers/AdminController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PostSharp.Patterns.Diagnostics;
+using PostSharp.Extensibility;
 
 namespace CarSharing.Views
 {
@@ -11,10 +13,23 @@ namespace CarSharing.Views
         //
         // GET: /Admin/
 
+
+        //[Log]
         public ActionResult Index()
         {
             return View();
         }
 
+        //[Log]
+        public void updateUserRights()
+        {
+            // update user rights in the database
+        }
+
+        //[Log]
+        public void getUserRights()
+        {
+            // read user rights from the database
+        }
     }
 }
