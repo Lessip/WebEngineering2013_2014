20781edf659cd257e848533feac23432a1482dcc
mashshu <mariya.shumbasova@gmail.com>
2014-01-26 18:23:04 +0100

    dynamic rss

---
+++ CarSharing/Controllers/RSSController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarSharing;
+using WebMatrix.WebData;
+using WebMatrix.Data;
+
+namespace CarSharing.Controllers
+{
+    public class RSSController : Controller
+    {
+        private CarSharingEntities db = new CarSharingEntities();
+
+        // GET: /RSS/
+        public ActionResult RSS()
+        {
+            Response.ContentType = "text/xml+rss";
+            return View("RSS");
+        }
+
+    }
+}
