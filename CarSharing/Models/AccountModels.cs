d77251eba5715d1eab95574d4149a8556b10115c
Patric <patric.lessmann@gmail.com>
2014-02-01 20:00:42 +0100

    Update - functionality for the user (view list, create, edit, detail, delete) and  added unique labels for the data (also in the cars-section)

--- CarSharing/Models/AccountModels.cs
+++ CarSharing/Models/AccountModels.cs
@@ -26,18 +26,31 @@ namespace CarSharing.Models
     {
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID")]
         public int id { get; set; }
+        [Display(Name = "Username")]
         public string login_name { get; set; }
+        [Display(Name = "First name")]
         public String firstname { get; set; }
+        [Display(Name = "Family Name")]
         public String name { get; set; }
-        public DateTime date_of_birth { get; set; }        
+        [Display(Name = "Date of birth")]
+        public DateTime date_of_birth { get; set; }
+        [Display(Name = "Password")]
         public String password { get; set; }
+        [Display(Name = "Email")]
         public String email { get; set; }
+        [Display(Name = "Access state")]
         public int access_state { get; set; }
+        [Display(Name = "Identity number")]
         public Nullable<Guid> identity_number { get; set; }
+        [Display(Name = "Timelimit")]
         public Nullable<DateTime> timelimit { get; set; }
+        [Display(Name = "Street")]
         public String street { get; set; }
+        [Display(Name = "Zip code")]
         public String post_code { get; set; }
+        [Display(Name = "City")]
         public String city { get; set; }
 
         // Constructors for new userprofiles
@@ -63,74 +76,74 @@ namespace CarSharing.Models
             street = new_user_address.street;
             post_code = new_user_address.post_code;
             city = new_user_address.city;
-        }        
-    }
-
-        public class RegisterExternalLoginModel
-        {
-            [Required]
-            [Display(Name = "Benutzername")]
-            public string UserName { get; set; }
-
-            public string ExternalLoginData { get; set; }
-        }
-
-        public class LocalPasswordModel
-        {
-            [Required]
-            [DataType(DataType.Password)]
-            [Display(Name = "Aktuelles Kennwort")]
-            public string OldPassword { get; set; }
-
-            [Required]
-            [StringLength(100, ErrorMessage = "&quote{0}&quote muss mindestens {2} Zeichen lang sein.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Neues Kennwort")]
-            public string NewPassword { get; set; }
-
-            [DataType(DataType.Password)]
-            [Display(Name = "Neues Kennwort bestätigen")]
-            [Compare("NewPassword", ErrorMessage = "Das neue Kennwort entspricht nicht dem Bestätigungskennwort.")]
-            public string ConfirmPassword { get; set; }
-        }
-
-        public class LoginModel
-        {
-            [Required]
-            [Display(Name = "Benutzername")]
-            public string UserName { get; set; }
-
-            [Required]
-            [DataType(DataType.Password)]
-            [Display(Name = "Kennwort")]
-            public string Password { get; set; }
-
-            [Display(Name = "Speichern?")]
-            public bool RememberMe { get; set; }
-        }
-
-        public class RegisterModel
-        {
-            [Required]
-            [Display(Name = "Benutzername")]
-            public string UserName { get; set; }
-
-            [Required]
-            [StringLength(100, ErrorMessage = "&quote{0}&quote muss mindestens {2} Zeichen lang sein.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Kennwort")]
-            public string Password { get; set; }
-
-            [DataType(DataType.Password)]
-            [Display(Name = "Kennwort bestätigen")]
-            [Compare("Password", ErrorMessage = "Das Kennwort entspricht nicht dem Bestätigungskennwort.")]
-            public string ConfirmPassword { get; set; }
-        }
-
-        public class ExternalLogin
-        {
-            public string Provider { get; set; }
-            public string ProviderDisplayName { get; set; }
-            public string ProviderUserId { get; set; }
         }
     }
+
+    public class RegisterExternalLoginModel
+    {
+        [Required]
+        [Display(Name = "Benutzername")]
+        public string UserName { get; set; }
+
+        public string ExternalLoginData { get; set; }
+    }
+
+    public class LocalPasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Aktuelles Kennwort")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "&quote{0}&quote muss mindestens {2} Zeichen lang sein.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Neues Kennwort")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Neues Kennwort bestätigen")]
+        [Compare("NewPassword", ErrorMessage = "Das neue Kennwort entspricht nicht dem Bestätigungskennwort.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Benutzername")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Kennwort")]
+        public string Password { get; set; }
+
+        [Display(Name = "Speichern?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "Benutzername")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "&quote{0}&quote muss mindestens {2} Zeichen lang sein.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Kennwort")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Kennwort bestätigen")]
+        [Compare("Password", ErrorMessage = "Das Kennwort entspricht nicht dem Bestätigungskennwort.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ExternalLogin
+    {
+        public string Provider { get; set; }
+        public string ProviderDisplayName { get; set; }
+        public string ProviderUserId { get; set; }
+    }
+}
