74d0a7098c282f34a41caa6593b66b5d3e83cba2
Patric <patric.lessmann@gmail.com>
2014-02-04 18:41:31 +0100

    Update - added model and view for time-simulation

---
+++ CarSharing/Models/TimeSimulation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarSharing.Models
+{
+    public class TimeSimulation
+    {
+        public List<CarSharing.Models.UserProfile> userTable;
+        public List<CarSharing.Models.CarProfile> carTable;
+        public List<CarSharing.contract> contractTable;
+
+        public TimeSimulation()
+        {
+            userTable = null;
+            carTable = null;
+            contractTable = null;
+        }
+
+        public TimeSimulation(List<CarSharing.Models.UserProfile> userList, List<CarSharing.Models.CarProfile> carList, List<contract> contractList)
+        {
+            userTable = userList;
+            carTable = carList;
+            contractTable = contractList;
+        }
+    }
+}
